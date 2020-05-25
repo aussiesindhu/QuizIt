fe8ab828aff8ab5401b8e61c11e82818fe42f4db
Mukunda <Sindhu.Mukunda1@T-Mobile.com>
2020-05-24 18:55:47 -0700

    Added levels and topics GET API to QuizController

--- api/src/main/java/quizIt/api/AdminController.java
+++ api/src/main/java/quizIt/api/AdminController.java
@@ -39,14 +39,6 @@ public class AdminController {
 		return data;			
 	}
 	
-	//get the list of all topics
-	@RequestMapping(value = "/admn/topics", method = RequestMethod.GET)
-	public HashSet<String> getTopic() {
-		for(Quizdata q:repo.findAll()) 
-				this.topics.add(q.getT());
-		return topics;
-	}
-	
 	//Post a new topic
 	@RequestMapping(value = "/admn/topics", method = RequestMethod.POST)
 	public void postTopic(@RequestBody String topic) {
--- api/src/main/java/quizIt/api/QuizController.java
+++ api/src/main/java/quizIt/api/QuizController.java
@@ -1,6 +1,7 @@
 package quizIt.api;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -23,12 +24,15 @@ public class QuizController {
 
 	public int qLen;
 	public Random r = new Random();
+	HashSet<String> topics = new HashSet<String>();
+	HashSet<String> levels = new HashSet<String>();
+
 
 	QuizController(QuizRepository repo) {
 		this.repo = repo;
 	}
 
-	//Get 1 random question for the chosen topic and level
+	// Get 1 random question for the chosen topic and level
 	@RequestMapping(value = "/{topic}/{level}", method = RequestMethod.GET)
 	public QSet getTopicLevel(@PathVariable("topic") String topic, @PathVariable("level") String level) {
 		List<Quizdata> res = new ArrayList<Quizdata>();
@@ -40,5 +44,22 @@ public class QuizController {
 		QSet qs = new QSet(randq.Q, randq.C, randq.A);
 		return qs;
 	}
+
+	// get the list of all topics
+	@RequestMapping(value = "/topics", method = RequestMethod.GET)
+	public HashSet<String> getTopic() {
+		for (Quizdata q : repo.findAll())
+			this.topics.add(q.getT());
+		return topics;
+	}
+	
+	//get levels
+	@RequestMapping(value = "/levels", method = RequestMethod.GET)
+	public HashSet<String> getLevelsl() {
+		for (Quizdata q : repo.findAll())
+			this.levels.add(q.getL());
+		return levels;
+	}
+	
 
 }
